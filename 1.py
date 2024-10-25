538b252be66140afe632817eb6d20ab4a297621b
narvarstrange <saksham63tt@gmail.com>
2024-10-26 01:29:31 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -181,4 +181,4 @@ handler = Mangum(app)  # Create the Mangum handler *outside* the if __name__ == 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
