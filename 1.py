14b89528cc896b18ccc3be261dd49aa66b986cce
Narvar Strange <saksham63tt@gmail.com>
2024-10-26 11:59:05 +0530

    Revert "Update 1.py"
    This reverts commit b506c71cb7d52098866119f3b671efbe78b8660f.

--- 1.py
+++ 1.py
@@ -181,6 +181,6 @@ async def root():
 
 handler = Mangum(app)
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
